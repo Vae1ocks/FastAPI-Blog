3664980b4d269336f8daee1655ec0b5eb793074f
Vae1ocks <antondobranickij@gmail.com>
2024-09-30 21:49:21 +0300

    Изменение модели User, изменение Config

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,8 +1,15 @@
-from sqlalchemy.ext.asyncio import AsyncSession
+from typing import TYPE_CHECKING
+
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from fastapi_users.db import SQLAlchemyBaseUserTable
 
 from src.models import Base
 
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
 
 class User(Base, SQLAlchemyBaseUserTable[int]):
-    pass
+    @classmethod
+    def get_db(cls, session: AsyncSession):
+        return SQLAlchemyUserDatabase(session, cls)
