e3c1622955bcbed4a809c7bd0795a161e8fa3642
Vae1ocks <antondobranickij@gmail.com>
2024-10-06 18:28:12 +0300

    UserLogin scheme

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 
 from annotated_types import MinLen, MaxLen
-from pydantic import BaseModel, EmailStr, ConfigDict
+from pydantic import BaseModel, EmailStr, ConfigDict, model_validator
 
 
 class BaseUser(BaseModel):
@@ -16,6 +16,18 @@ class CreateUser(BaseUser):
 class UserRead(BaseUser):
     class Config:
         from_attributes = True
+
+
+class UserLogin(BaseModel):
+    email: EmailStr | None = None
+    username: Annotated[str, MinLen(3), MaxLen(25)] | None = None
+    password: Annotated[str, MinLen(8)]
+
+    @model_validator(mode="after")
+    def check_email_or_username_provided(self):
+        if not self.email and not self.username:
+            raise ValueError("Neither email nor username provided")
+        return self
 
 
 class Code(BaseModel):
