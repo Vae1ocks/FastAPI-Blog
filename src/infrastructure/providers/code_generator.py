3904ba75809d957a25e49dfb225f51a4d11f6e58
Vae1ocks <antondobranickij@gmail.com>
2024-11-25 18:11:18 +0300

    code generator provider

---
+++ src/infrastructure/providers/code_generator.py
@@ -0,0 +1,12 @@
+from random import randint
+
+from application.providers.code_generator import CodeIntGenerator
+
+
+class CodeIntGeneratorImpl(CodeIntGenerator):
+    def __init__(self, min_val: int, max_val: int):
+        self.min_val = min_val
+        self.max_val = max_val
+
+    def __call__(self):
+        return randint(self.min_val, self.max_val)
