1c4522a75a6df02c084d369599713503842e7d2b
Vae1ocks <antondobranickij@gmail.com>
2024-11-25 21:38:11 +0300

    user repo added and changed methods

--- src/domain/repositories/user_repository.py
+++ src/domain/repositories/user_repository.py
@@ -4,17 +4,30 @@ from domain.entities.user.models import User, UserId
 
 
 class UserRepository(Protocol):
-    async def get_by_id(self, user_id: UserId) -> User | None:
-        ...
+    async def get_by_id(self, user_id: UserId) -> User | None: ...
 
-    async def get_by_username(self, username: str, status: str | None) -> User:
-        ...
+    async def get_by_username(
+        self,
+        username: str,
+        is_active: bool | None = None,
+        is_confirmed: bool | None = None,
+    ) -> User: ...
 
-    async def get_by_email(self, email: str, status: str | None) -> User:
-        ...
+    async def get_by_email(
+        self,
+        email: str,
+        is_active: bool | None = None,
+        is_confirmed: bool | None = None,
+    ) -> User: ...
 
-    async def add(self, user: User) -> None:
-        ...
+    async def get_by_email_or_username(
+        self,
+        email: str,
+        username: str,
+        is_active: bool | None = None,
+        is_confirmed: bool | None = None,
+    ) -> list[User]: ...
 
-    async def delete(self, user: User) -> None:
-        ...
+    async def add(self, user: User) -> None: ...
+
+    async def delete(self, user: User) -> None: ...
--- src/infrastructure/repositories/sqlalchemy/user_repository.py
+++ src/infrastructure/repositories/sqlalchemy/user_repository.py
@@ -1,34 +1,71 @@
-from sqlalchemy import select
+from sqlalchemy import select, Select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from domain.entities.user.models import User, UserId
 
 
-class UserRepository:
+def add_is_conf_is_active_to_stmt(
+    stmt: Select, is_active: bool, is_confirmed: bool
+):
+    if is_active is not None:
+        stmt = stmt.where(User.is_active == is_active)  # type: ignore
+    if is_confirmed is not None:
+        stmt = stmt.where(User.is_confirmed == is_confirmed)  # type: ignore
+    return stmt
+
+
+class UserRepositoryImpl:
     def __init__(self, session: AsyncSession):
         self.session = session
 
     async def get_by_id(self, user_id: UserId) -> User | None:
-        stmt = select(User).where(User.id == user_id)
+        stmt = select(User).where(User.id == user_id)  # type: ignore
         return (await self.session.execute(stmt)).scalar_one_or_none()
 
-    async def get_by_username(self, username: str, status: str | None) -> User:
-        stmt = select(User).where(
-            User.username == username, User.status == status
+    async def get_by_username(
+        self,
+        username: str,
+        is_active: bool | None = None,
+        is_confirmed: bool | None = None,
+    ) -> User:
+        stmt = select(User).where(User.username == username)  # type: ignore
+        stmt = add_is_conf_is_active_to_stmt(
+            stmt=stmt, is_active=is_active, is_confirmed=is_confirmed
         )
         return (await self.session.execute(stmt)).scalar_one_or_none()
 
-    async def get_by_email(self, email: str, status: str | None) -> User:
-        stmt = select(User).where(
-            User.email == email, User.status == status
+    async def get_by_email(
+        self,
+        email: str,
+        is_active: bool | None = None,
+        is_confirmed: bool | None = None,
+    ) -> User:
+        stmt = select(User).where(User.email == email)  # type: ignore
+        stmt = add_is_conf_is_active_to_stmt(
+            stmt=stmt, is_active=is_active, is_confirmed=is_confirmed
         )
         return (await self.session.execute(stmt)).scalar_one_or_none()
+
+    async def get_by_email_or_username(
+        self,
+        email: str,
+        username: str,
+        is_active: bool | None = None,
+        is_confirmed: bool | None = None,
+    ) -> list[User]:
+        stmt = select(User).where(
+            (User.email == email) | (User.username == username)  # type: ignore
+        )
+        stmt = add_is_conf_is_active_to_stmt(
+            stmt=stmt, is_active=is_active, is_confirmed=is_confirmed
+        )
+        return list((await self.session.execute(stmt)).scalars())
 
     async def add(self, user: User) -> None:
         self.session.add(user)
 
     async def delete(self, user_id: UserId) -> None:
-        stmt = select(User).where(User.id == user_id)
+        stmt = select(User).where(User.id == user_id)  # type: ignore
         user = (await self.session.execute(stmt)).scalar_one_or_none()
         if user is None:
             raise ValueError(f"User with id={user_id} does not exist.")
