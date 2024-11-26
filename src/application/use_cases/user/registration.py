28396cb73f1a102cc8afa06a00c55a16d6beb0c6
Vae1ocks <antondobranickij@gmail.com>
2024-11-26 20:53:37 +0300

    registration UseCase

---
+++ src/application/use_cases/user/registration.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+
+from application.dto.user.user_read import UserReadDTO
+from application.mappers.user.user_to_dto import UserToDTOMapper
+from domain.entities.user.models import User
+from application.dto.user.user_create import UserCreateDTO
+from application.providers.code_generator import RandomCodeGenerator
+from application.services.user_registration import UserRegistrationService
+from application.uow import UnitOfWork
+from infrastructure.providers.email_sender import EmailSender
+
+
+@dataclass
+class RegistrationUseCase:
+    registration_service: UserRegistrationService
+    user_to_dto_mapper: UserToDTOMapper
+    code_generator: RandomCodeGenerator
+    email_sender: EmailSender
+    uow: UnitOfWork
+
+    async def registrate_unconfirmed(self, dto: UserCreateDTO) -> tuple[UserReadDTO, int]:
+        async with self.uow as uow:
+            user = await self.registration_service.register_unconfirmed(
+                uow=uow, data=dto
+            )
+            code = self.code_generator()
+            message = f"Your confirmation code to process the registration: {code}"
+            self.email_sender.send(
+                emails=[user.email],
+                subject="Confirmation code",
+                message=message,
+            )
+            user_dto = self.user_to_dto_mapper.to_read_dto(user)
+            return user_dto, code
