53d6e90e5b0ab68ccea391fdef448a2a83130f3a
Vae1ocks <antondobranickij@gmail.com>
2024-11-27 20:22:23 +0300

    split registration usecase into 2

--- src/application/use_cases/user/registration.py
+++ src/application/use_cases/user/registration.py
@@ -18,7 +18,7 @@ class RegistrationUseCase:
     email_sender: EmailSender
     uow: UnitOfWork
 
-    async def registrate_unconfirmed(self, dto: UserCreateDTO) -> tuple[UserReadDTO, int]:
+    async def execute(self, dto: UserCreateDTO) -> tuple[UserReadDTO, int]:
         async with self.uow as uow:
             user = await self.registration_service.register_unconfirmed(
                 uow=uow, data=dto
@@ -33,7 +33,14 @@ class RegistrationUseCase:
             user_dto = self.user_to_dto_mapper.to_read_dto(user)
             return user_dto, code
 
-    async def confirm_registration(self, dto: ConfirmationCodesDTO) -> UserReadDTO:
+
+@dataclass
+class RegistrationConfirmationUseCase:
+    registration_service: UserRegistrationService
+    user_to_dto_mapper: UserToDTOMapper
+    uow: UnitOfWork
+
+    async def execute(self, dto: ConfirmationCodesDTO) -> UserReadDTO:
         async with self.uow as uow:
             user = await self.registration_service.confirm_user(uow=uow, data=dto)
             user_dto = self.user_to_dto_mapper.to_read_dto(user)
