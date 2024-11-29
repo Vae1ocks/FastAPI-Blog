96a2a1a672aa9cf55e2be193c21a4933e87067bd
Vae1ocks <antondobranickij@gmail.com>
2024-11-30 00:08:24 +0300

    infrastructure container

---
+++ src/setup/di_containers/infrastructure.py
@@ -0,0 +1,33 @@
+from dependency_injector.containers import DeclarativeContainer
+from dependency_injector.providers import Singleton, Factory
+
+from infrastructure.providers.code_generator import RandomIntegerCodeGenerator
+from infrastructure.providers.file_operators import (
+    ImageCheckerImpl,
+    FileSystemImageLoader,
+)
+from infrastructure.providers.password_hasher_bcrypt import BcryptPasswordHasher
+from setup.configs import configs
+
+
+class InfrastructureContainer(DeclarativeContainer):
+    password_hasher = Factory(
+        BcryptPasswordHasher,
+        pepper=configs.pepper.pepper,
+    )
+
+    image_checker = Singleton(ImageCheckerImpl)
+    image_loader = Factory(
+        FileSystemImageLoader,
+        directory=configs.file.users_profile_images_directory,
+        file_name_patter=configs.file.file_save_time_pattern,
+    )
+
+    code_generator = Singleton(
+        RandomIntegerCodeGenerator,
+        min_val=configs.code_generator.min_val,
+        max_val=configs.code_generator.max_val,
+    )
+
+
+infrastructure_container = InfrastructureContainer()
