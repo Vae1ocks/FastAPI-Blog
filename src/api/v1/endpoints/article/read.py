24de28d2d92683c8c7e54fbc990cd971fea84fba
Vae1ocks <antondobranickij@gmail.com>
2024-12-27 14:41:19 +0300

    Article detail read

--- src/api/v1/endpoints/article/read.py
+++ src/api/v1/endpoints/article/read.py
@@ -1,9 +1,13 @@
 from dishka import FromDishka
 from dishka.integrations.fastapi import inject
 
-from application.dto.article.article_read import ArticleListDTO
+from application.dto.article.article_read import ArticleListDTO, ArticleReadDTO
 from application.dto.common.list import ListObjectsDTO
-from application.use_cases.article.read import ArticleListUseCase
+from application.use_cases.article.read import (
+    ArticleListUseCase,
+    ArticleReadDetailUseCase,
+)
+from domain.entities.article.models import ArticleId
 from .router import router
 from ...mappers.article.dto_to_scheme import ArticleDTOToSchemeMapper
 from ...mappers.common.list_objects_mapper import ListObjectsOrderMapper
@@ -25,3 +29,12 @@ async def list_articles(
     list_dtos: list[ArticleListDTO] = await list_usecase.execute(dto=list_dto)
     return ArticleDTOToSchemeMapper.to_list_scheme(dtos=list_dtos)
 
+
+@router.get("/{article_id}")
+@inject
+async def article_read(
+    article_id: ArticleId,
+    detail_usecase: FromDishka[ArticleReadDetailUseCase],
+):
+    read_dto: ArticleReadDTO = await detail_usecase.execute(article_id=article_id)
+    return ArticleDTOToSchemeMapper.to_read_scheme(dto=read_dto)
--- src/application/use_cases/article/read.py
+++ src/application/use_cases/article/read.py
@@ -2,10 +2,19 @@ from dataclasses import dataclass
 
 from application.dto.article.article_read import ArticleReadDTO, ArticleListDTO
 from application.dto.common.list import ListObjectsDTO
-from application.errors.common.not_found import DoesNotExist
 from application.mappers.article.model_to_dto import ArticleModelToDTOMapper
 from application.services.article.read import ArticleReadService
 from domain.entities.article.models import ArticleId, Article
+
+
+@dataclass
+class ArticleReadDetailUseCase:
+    read_service: ArticleReadService
+
+    async def execute(self, article_id: ArticleId) -> ArticleReadDTO:
+        article: Article = await self.read_service.read_by_id(article_id)
+        dto: ArticleReadDTO = ArticleModelToDTOMapper.to_read_dto(model=article)
+        return dto
 
 
 @dataclass
